73fa2155877a8b30fe80b110b736de50f124e542
ShaharKeret12 <shaharkeret12@gmail.com>
2025-11-09 09:28:20 +0200

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "resources.premierleague.com" },
-    ],
-  },
-};
-export default nextConfig;
