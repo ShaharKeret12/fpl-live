c414b37c995432397a9218ecb2c8a0bdd1ddd9c4
ShaharKeret12 <shaharkeret12@gmail.com>
2025-11-09 09:36:47 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import FPLLive from "@/components/FPLLive";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen w-full bg-neutral-50 text-neutral-900 p-6">
+      <div className="max-w-5xl mx-auto">
+        <FPLLive />
+      </div>
+    </main>
+  );
+}
